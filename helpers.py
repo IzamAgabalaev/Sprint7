bcd778c0cdeabed1d1ac3aa0d7fa37c273b5eee2
Izam Agabalaev <izamagabalaev592@gmail.com>
2025-02-13 12:47:21 +0500

    Добавил фикстуру создания и удаления курьера

--- helpers.py
+++ helpers.py
@@ -1,4 +1,7 @@
 from faker import Faker
+import requests
+import pytest
+from urls import Urls
 
 fake = Faker()
 fakeRU = Faker(locale='ru_RU')
@@ -17,3 +20,25 @@ def create_random_password():
 def create_random_firstname():
     first_name = fakeRU.first_name()
     return first_name
+
+
+@pytest.fixture
+def create_courier():
+    payload = {
+        'login': create_random_login(),
+        'password': create_random_password(),
+        'firstName': create_random_firstname()
+    }
+    response = requests.post(Urls.URL_courier_create, data=payload)
+    assert response.status_code == 201 and response.json() == {'ok': True}
+
+    courier_id = requests.post(Urls.URL_courier_login, data={
+        'login': payload['login'],
+        'password': payload['password']
+    }).json().get('id')
+
+    yield payload, courier_id
+
+    if courier_id:
+        delete_response = requests.delete(f"{Urls.URL_courier_delete.replace(':id', str(courier_id))}")
+        assert delete_response.status_code == 200 and response.json() == {'ok': True}
--- tests/test_courier_create.py
+++ tests/test_courier_create.py
@@ -3,33 +3,23 @@ import allure
 import pytest
 from data import Data
 from urls import Urls
-from helpers import create_random_login, create_random_password, create_random_firstname
-
+from helpers import create_random_login, create_random_password, create_random_firstname, create_courier
 
 class TestCourierCreate:
 
     @allure.title('Создание аккаунта с валидными данными')
-    def test_create_courier_success_valid_data(self):
-        payload = {
-            'login': create_random_login(),
-            'password': create_random_password(),
-            'firstName': create_random_firstname()
-        }
-        response = requests.post(Urls.URL_courier_create, data=payload)
-        assert response.status_code == 201 and response.json() == {'ok': True}
+    def test_create_courier_success_valid_data(self, create_courier):
+        payload, _ = create_courier
+        assert payload is not None
 
-    @allure.title('Повторное использовании логина для создания курьера')
-    def test_create_courier_duplicate_login_error(self):
-        payload = {
-            'login': Data.valid_login,
-            'password': create_random_password(),
-            'firstName': create_random_firstname()
-        }
+    @allure.title('Повторное использование логина для создания курьера')
+    def test_create_courier_duplicate_login_error(self, create_courier):
+        payload, _ = create_courier
         response = requests.post(Urls.URL_courier_create, data=payload)
         assert response.status_code == 409
         assert response.json()['message'] == Data.error_messages['duplicate_login']
 
-    @allure.title('Незаполненные обязательными поля')
+    @allure.title('Незаполненные обязательные поля')
     @pytest.mark.parametrize('empty_credentials', [
         {'login': '', 'password': create_random_password(), 'firstName': create_random_firstname()},
         {'login': create_random_login(), 'password': '', 'firstName': create_random_firstname()}
